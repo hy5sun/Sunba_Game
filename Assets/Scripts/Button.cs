24cdc4caae99be04bf8325db7c162c9d1eb691d2
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2022-07-23 02:01:01 +0900

    2022.07.19
    feat: 마우스를 오브젝트 위에 올려두면 옆에 작은 화면을 띄우는 기능 추가하기
    refactor: button 스크립트
    style: 옷 입히기 창 GUI 배치/디자인, 평가 창 GUI 배치
    Co-Authored-By: kimsuin <87413903+lsuinl@users.noreply.github.com>

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -9,25 +9,30 @@ public class Button : MonoBehaviour //특정 버튼을 누르면 씬이 변경되는 스크립트
     public void OnClickStart() //게임시작 버튼을 누르면
     {
         Debug.Log("게임을 시작합니다!");
-        SceneManager.LoadScene(2); //스토리 고르는 창으로 이동
+        SceneManager.LoadScene("Choose_stage"); //스토리 고르는 창으로 이동
     }
 
     public void OnClickHow() //게임설명 버튼을 누르면
     {
         Debug.Log("설명창으로 이동합니다.");
-        SceneManager.LoadScene(1); //설명창으로 이동
+        SceneManager.LoadScene("Description"); //설명창으로 이동
     }
 
     public void OnClickMain() //처음으로 버튼을 누르면
     {
         Debug.Log("메인화면으로 이동합니다.");
-        SceneManager.LoadScene(0); //메인화면으로 이동
+        SceneManager.LoadScene("Main Menu"); //메인화면으로 이동
     }
 
     public void OnClickClothes() //사용자가 스토리를 선택하면
     {
         Debug.Log("잠깐만 기다료봐~");
-        SceneManager.LoadScene(3); //옷 입히는 게임 창으로 이동
+        SceneManager.LoadScene("main_clothes"); //옷 입히는 게임 창으로 이동
+    }
+
+    public void OnClickreplay() //사용자가 다시하기 버튼을 누르면
+    {
+        SceneManager.LoadScene("Choose_stage"); //스토리 선택 화면으로 이동
     }
 
     // Start is called before the first frame update
--- Assets/Scripts/story_telling.cs
+++ Assets/Scripts/story_telling.cs
@@ -13,14 +13,28 @@ public class story_telling : MonoBehaviour
 
     void Update()
     {
-        if (EventSystem.current.IsPointerOverGameObject() ==true) //마우스가 오브젝트 위에 있을 떄
-        {
-            Debug.Log("응애 나 눌리는 중");
-            transform.GetChild(0).gameObject.SetActive(true);
-        }
-        else
-        {
-            transform.GetChild(0).gameObject.SetActive(false);
-        }
+    //    if (EventSystem.current.IsPointerOverGameObject() ==true) //마우스가 오브젝트 위에 있을 떄
+    //    {
+    //        Debug.Log("응애 나 눌리는 중");
+
+    //        //어떤 오브젝트 위에 있는지 확인.
+    //        //해당 오브젝트의 이름을 변수에 저장
+    //                                //변수이름.여기에
+    //        transform.GetChild(0).gameObject.SetActive(true);
+    //    }
+    //    else
+    //    {
+    //        transform.GetChild(0).gameObject.SetActive(false);
+    //    }
+    }
+    private void OnMouseEnter()
+    {
+        Debug.Log("응애 나 눌리는 중");
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
