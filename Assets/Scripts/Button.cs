8aacc11c7b49744f18408e5628f68f52d0e53368
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2022-08-13 17:12:35 +0900

    refactor: Button.cs 수정. 세트 선택 시 머리 비활 ↔ 머리 선택 시 세트 비활 / style: 옷 이미지 위치 조정

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,7 +6,10 @@ using UnityEngine.EventSystems; //커서 위치 감지
 
 public class Button : MonoBehaviour //특정 버튼을 누르면 주어진 코드가 동작하는 스크립트
 {
-    [SerializeField] GameObject img; //나타나게 할 이미지 지정 (옷 선택할 때)
+    public GameObject img; //나타나게 할 이미지 지정 (옷 선택할 때)
+    public GameObject story;
+    public static int _score; //총 점수
+    public static string _stage; //현재 스테이지 (옷 계산할 때 사용)
 
     public void OnClickStart() //게임시작 버튼을 누르면
     {
@@ -28,28 +31,69 @@ public class Button : MonoBehaviour //특정 버튼을 누르면 주어진 코드가 동작하는 
 
     public void OnClickStory() //사용자가 스토리를 선택하면
     {
-        Debug.Log("잠깐만 기다료봐~");
+        Debug.Log("옷 입히기 씬으로 이동중!");
+        _stage = story.tag;
+
         SceneManager.LoadScene("main_clothes"); //옷 입히는 게임 창으로 이동
     }
 
-    public void OnClickreplay() //사용자가 다시하기 버튼을 누르면
+    public void ending() //사용자가 완료 버튼을 누르면
     {
-        SceneManager.LoadScene("Choose_stage"); //스토리 선택 화면으로 이동
+        
+        Debug.Log("완성!!!");
+
+        //완료 버튼을 누르면 활성화 되어있는 오브젝트들의 점수 합산하기
+        string[] tagg = { "hair", "bottom", "top", "acc", "shoes" };
+        foreach (string k in tagg)
+        {
+            GameObject[] category = GameObject.FindGameObjectsWithTag(k);
+            
+            for (int i = 0; i < category.Length; i++)
+            {
+                if (category[i].gameObject.activeSelf == true)
+                {
+                    category[i].GetComponent<score>().plusScore(_stage); //score 스크립트 속 plusScore 함수 실행
+                }
+            }
+        }
+        
+        SceneManager.LoadScene("resultscene"); //결과 발표 창으로 이동
     }
+
 
     public void OnClickClothes() //사용자가 원하는 옷을 클릭하면
     {
         string tagg = img.tag;
-        var t = GameObject.FindGameObjectsWithTag(tagg);
-        for(int i = 0; i < t.Length; i++)
+        GameObject set = GameObject.Find("top").transform.Find("top3").gameObject;
+
+        if (tagg == "bottom") //하의일경우 세트 비활성화
+            set.gameObject.SetActive(false);
+
+        if (tagg == "hair") //머리 적용한 경우 세트 비활성화
+            set.gameObject.SetActive(false);
+
+        var category = GameObject.FindGameObjectsWithTag(tagg);
+        for (int i = 0; i < category.Length; i++)
         {
-            if(t[i].gameObject.activeSelf == true){
-                t[i].gameObject.SetActive(false);
-                break;
-            }
+            category[i].gameObject.SetActive(false);
         }
 
         img.SetActive(true); //사용자가 클릭한 옷 그림 활성화
+
+        if (set.gameObject.activeSelf == true)
+        { //상의가 세트인경우 하의, 머리 비활성화
+            category = GameObject.FindGameObjectsWithTag("bottom");
+            for (int i = 0; i < category.Length; i++)
+            {
+                category[i].gameObject.SetActive(false);
+            }
+
+            category = GameObject.FindGameObjectsWithTag("hair");
+            for (int i = 0; i < category.Length; i++)
+            {
+                category[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     public void on() //오브젝트 위에 마우스를 올리면 (choose_stage Scene)
@@ -61,5 +105,17 @@ public class Button : MonoBehaviour //특정 버튼을 누르면 주어진 코드가 동작하는 
     {
         transform.GetChild(0).gameObject.SetActive(false); //child 0을 비활성화
     }
-
+    public void resetting()
+    {//리셋버튼을 누르면
+        
+        string[] tagg = { "hair", "top", "bottom", "acc", "shoes" };
+        foreach (string e in tagg) {
+            var category = GameObject.FindGameObjectsWithTag(e);
+            for (int i = 0; i < category.Length; i++)
+            {
+                if (category[i].gameObject.activeSelf == true)
+                    category[i].gameObject.SetActive(false);
+            }
+        }
+    }
 }
